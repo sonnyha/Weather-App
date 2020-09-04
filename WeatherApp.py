a5224fbe667065e76d6d6c42a8728a356a7d0ecd
sonnyha <sonnyha@live.com>
2020-09-04 14:00:08 -0500

    Update WeatherApp.py

--- WeatherApp.py
+++ WeatherApp.py
@@ -1,14 +1,17 @@
 import tkinter as tk
 
+# defining height and width for the arguments (const)
 HEIGHT = 700
 WIDTH = 800
 
 # root window
 root = tk.Tk()
 
+# placeholder to get initial screen size
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
+# to start organizing the widgets you put on screen
 frame = tk.Frame(root, bg='#e6e6e6')
 frame.place(relwidth = 0.8, relheight = 0.8, relx = 0.1, rely = 0.1)
 
@@ -19,3 +22,6 @@ button.pack()
 # to run your application
 root.mainloop()
 
+"""
+
+"""
