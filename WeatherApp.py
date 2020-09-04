132b0cef5a9d7bf426c3b1dce5a56ff343f53630
sonnyha <sonnyha@live.com>
2020-09-04 13:49:12 -0500

    Update WeatherApp.py

--- WeatherApp.py
+++ WeatherApp.py
@@ -12,10 +12,10 @@ canvas.pack()
 frame = tk.Frame(root, bg='#e6e6e6')
 frame.place(relwidth = 0.8, relheight = 0.8, relx = 0.1, rely = 0.1)
 
+# button widget
 button = tk.Button(root, text="Test Button", bg='blue')
 button.pack()
 
 # to run your application
 root.mainloop()
 
-# testing one two three!
